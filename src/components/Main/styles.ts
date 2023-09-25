fca982073e423a5f174d9dc787ff51f7e6bc7042
Thalles Ian <thallesyam@gmail.com>
2023-09-25 11:11:38 -0300

    style: adjusting padding

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -7,7 +7,7 @@ export const Wrapper = styled.main`
   color: #ffffff;
   width: 100%;
   height: 100%;
-  padding: 3rem;
+  padding: 2rem;
   text-align: center;
   display: flex;
   flex-direction: column;
