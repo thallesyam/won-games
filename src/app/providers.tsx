125a42e70afbdb452a102c08a9cb11f80b11e915
Thalles Ian <thallesyam@gmail.com>
2023-09-26 09:20:54 -0300

    feat: config provider to global theme

---
+++ src/app/providers.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { PropsWithChildren } from 'react'
+
+import GlobalStyles from '@/styles/global'
+
+export function Providers({ children }: PropsWithChildren) {
+  return (
+    <>
+      <GlobalStyles />
+      {children}
+    </>
+  )
+}
