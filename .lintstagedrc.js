c2b98a7a35caab853d9d1670fac1f77048629554
thallesyam <thallesyam@gmail.com>
2023-09-28 16:42:04 -0300

    chore: config lintstaged path

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': (filenames) => [
-    `prettier --write '${filenames.join(' ')}'`,
-    `npm run lint --fix . '${filenames.join('--file')}'`,
-    `npm test -- --findRelatedTests '${filenames.join(' ')}'`
+    `prettier --write "${filenames.join('" "')}"`,
+    `npm run lint --fix . "${filenames.join('" --file "')}"`,
+    `npm test -- --findRelatedTests "${filenames.join('" "')}"`
   ]
 }
