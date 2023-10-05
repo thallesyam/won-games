53650cfa49a0aad2a705842a0ffc4d4d863e0a5e
Thalles Silva <thallesyam@DESKTOP-S2FIV5R.localdomain>
2023-10-05 19:20:39 -0300

    styles: config theme and types

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,12 +3,14 @@
 import { PropsWithChildren } from 'react'
 
 import GlobalStyles from '@/styles/global'
+import { ThemeProvider } from 'styled-components'
+import theme from '@/styles/theme'
 
 export function Providers({ children }: PropsWithChildren) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       {children}
-    </>
+    </ThemeProvider>
   )
 }
--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -3,8 +3,8 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.main`
-  background-color: #06092b;
-  color: #ffffff;
+  background-color: ${(props) => props.theme.colors.mainBg};
+  color: ${(props) => props.theme.colors.white};
   width: 100%;
   height: 100%;
   padding: 2rem;
