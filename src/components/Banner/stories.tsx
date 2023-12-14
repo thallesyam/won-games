e69b8918cca43ca683df73bb90589b18e308abbb
Thalles Ian <thallesyam@gmail.com>
2023-12-14 18:24:59 -0300

    wip: banner module

---
+++ src/components/Banner/stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryFn } from '@storybook/react'
+import Banner, { BannerProps } from '.'
+
+export default {
+  title: 'Banner',
+  component: Banner,
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  },
+  parameters: {
+    layout: 'fullscream'
+  }
+} as Meta
+
+export const Default: StoryFn<BannerProps> = (args) => <Banner {...args} />
