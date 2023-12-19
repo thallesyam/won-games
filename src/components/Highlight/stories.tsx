fa8de4db5ec8506a8e491b2e5a376fa79e26f024
Thalles Ian <thalles.silva@webmotors.com.br>
2023-12-19 16:52:08 -0300

    feat: create Highlight component

---
+++ src/components/Highlight/stories.tsx
@@ -0,0 +1,30 @@
+import { StoryFn, Meta } from '@storybook/react'
+import Highlight, { HighlightProps } from '.'
+
+export default {
+  title: 'Highlight',
+  component: Highlight,
+  args: {
+    title: 'Read Dead is back!',
+    subtitle: 'Come see John’s new adventures',
+    backgroundImage: '/img/red-dead-img.jpg',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/rdr2'
+  }
+} as Meta
+
+export const Default: StoryFn<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+export const WithFloatImage: StoryFn<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+WithFloatImage.args = {
+  floatImage: '/img/red-dead-float.png'
+}
